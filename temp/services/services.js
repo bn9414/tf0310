f0a25f6a7709d38aa03f55cd35a81b772321f101
Bhuvanesh <bhuvaneshnick@gmail.com>
2017-11-11 23:17:56 +0530

    sd

--- temp/services/services.js
+++ temp/services/services.js
@@ -1,3 +1,15 @@
-var x=document.getElementsByClassName('t12');
+var x=document.getElementsByClassName('quest');
+var y=document.getElementsByClassName('qopen');
+var tick=0;
+function open(){
+if(tick==0){    
+    y[0].style.display="block";
+}
+        if(tick==1){    
+    y[0].style.display="none";
+}
+tick=!tick;
+}
+x[0].addEventListener("click",open, false);
 
 
