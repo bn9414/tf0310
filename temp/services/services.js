a62929758bd9b0ebad80bb887dcc22a437e75272
Bhuvanesh <bhuvaneshnick@gmail.com>
2017-11-12 10:45:55 +0530

    sd

--- temp/services/services.js
+++ temp/services/services.js
@@ -1,17 +1,31 @@
 var x=document.getElementsByClassName('quest');
 var y=document.getElementsByClassName('qopen');
-var z=document.getElementById('close');
+var z=document.getElementsByClassName('close');
 var tick=0;
-function open(){
-if(tick==0){    
-    y[0].style.display="block";
-}
-        if(tick==1){    
+function open(n){
+if(n==0||n==1||n==2||n==3){ 
     y[0].style.display="none";
+    y[1].style.display="none";
+    y[2].style.display="none";
+    y[3].style.display="none";
+    
+    y[n].style.display="block";
+}
+        if(n==5){    
+    y[0].style.display="none";
+            y[1].style.display="none";
+            y[2].style.display="none";
+            y[3].style.display="none";
 }
 tick=!tick;
 }
-x[0].addEventListener("click",open, false);
-z.addEventListener("click",open, false);
+x[0].addEventListener("click",function(){open(0);}, false);
+x[1].addEventListener("click",function(){open(1);}, false);
+x[2].addEventListener("click",function(){open(2);}, false);
+x[3].addEventListener("click",function(){open(3);}, false);
+z[0].addEventListener("click",function(){open(5);}, false);
+z[1].addEventListener("click",function(){open(5);}, false);
+z[2].addEventListener("click",function(){open(5);}, false);
+z[3].addEventListener("click",function(){open(5);}, false);
 
 
