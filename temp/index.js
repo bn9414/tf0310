518d58dfd106d47a26677e53dd752d5f54c83256
Bhuvanesh <bhuvaneshnick@gmail.com>
2017-11-25 11:18:55 +0530

    sdd

--- temp/index.js
+++ temp/index.js
@@ -38,4 +38,22 @@ less[1].addEventListener('click',function(){benvisc(1);},false);
 less[2].addEventListener('click',function(){benvisc(2);},false);
 less[3].addEventListener('click',function(){benvisc(3);},false);
 
+/* start:section 1*/
+var what=document.getElementsByClassName('what');
+var slide_con=document.getElementsByClassName('slide_con');
 
+window.onscroll = function() {scrollani()};
+
+function scrollani() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        what[0].getElementsByTagName('img')[0].className='img_mob_move';
+         what[0].className='what what_mob_move';
+    slide_con[0].style.bottom="250px";
+    
+    }
+        
+}
+
+
+
+/* end:section 1*/
