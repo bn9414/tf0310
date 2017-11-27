d933430bc1812ff7fbb6bb9ae4e9989202d6dbea
Bhuvanesh <bhuvaneshnick@gmail.com>
2017-11-27 12:08:44 +0530

    df

--- temp/index.js
+++ temp/index.js
@@ -1,3 +1,14 @@
+
+//paste this code under the head tag or in a separate js file.
+	// Wait for window load
+	$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+	});
+
+
+
+
 var area=document.getElementsByClassName('area');
 var click=document.getElementsByClassName('click');
 var more=document.getElementsByClassName('more');
