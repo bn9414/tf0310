d3e146c65e127fcd6eb47f52f16b7c0ac3acc77e
Bhuvanesh <bhuvaneshnick@gmail.com>
2017-11-27 13:09:38 +0530

    sd

--- temp/index.js
+++ temp/index.js
@@ -6,10 +6,23 @@
 		$(".se-pre-con").fadeOut("slow");;
         
         var hd1=document.getElementsByClassName('hd1');
+         var hd2=document.getElementsByClassName('hd2');
+         var hd3=document.getElementsByClassName('hd3');
+         var hd4=document.getElementsByClassName('hd4');
+         var hd5=document.getElementsByClassName('hd5');
+         var hd6=document.getElementsByClassName('hd6');
+         var hd7=document.getElementsByClassName('hd7');
+        
 
 function homeani()
 {
     hd1[0].className=('hd1 homeani');
+    hd2[0].className=('hd2 homeani');
+    hd3[0].className=('hd3 homeani');
+    hd4[0].className=('hd4 homeani');
+    hd5[0].className=('hd5 homeani');
+    hd6[0].className=('hd6 homeani');
+    hd7[0].className=('hd7 homeani');
     
 }
 homeani();
