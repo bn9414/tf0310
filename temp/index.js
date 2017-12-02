76a2260b8f4a9eee3c98552346419396cfd6cca3
Bhuvanesh <bhuvaneshnick@gmail.com>
2017-12-02 23:25:17 +0530

    sd

--- temp/index.js
+++ temp/index.js
@@ -56,7 +56,8 @@ var s_prev=0;
 
 window.onscroll = function() {scrollani()};
 
-
+  var h = parseInt(window.innerHeight);
+        var w = parseInt(window.innerWidth);
 
 function scrollani() {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
@@ -71,7 +72,10 @@ function scrollani() {
       
     }
     
-     if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+    
+     if(w>500){
+    
+     if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
        
          stick_c[0].style.opacity="1";
          ncr[0].style.paddingLeft="30px";
@@ -84,15 +88,12 @@ function scrollani() {
          lcright[2].style.height="0px";
              scright[0].style.opacity="1";
         scright[0].style.top="0px";
-         
-         
-    
+
     }
-    
-    if (document.body.scrollTop > 1400 || document.documentElement.scrollTop > 1400) {
+
+    if (document.body.scrollTop > 1300 || document.documentElement.scrollTop > 1300) {
      
-        
-        
+
            ncl[1].style.paddingRight="30px";
          ncl[1].style.opacity="1";
           ncr[1].style.paddingLeft="30px";
@@ -116,16 +117,11 @@ function scrollani() {
         scleft[1].style.top="0px";
             scright[1].style.opacity="1";
         scright[1].style.top="0px";
-        
-        
-           
-        
+    
         
     }
-    
-    
-    
-       if (document.body.scrollTop > 2000 || document.documentElement.scrollTop > 2000) {
+
+       if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
        
          
          ncr[2].style.paddingLeft="30px";
@@ -138,13 +134,11 @@ function scrollani() {
          lcright[8].style.height="0px";
              scright[2].style.opacity="1";
         scright[2].style.top="0px";
-         
-         
-    
+
     }
     
     
-     if (document.body.scrollTop > 2400 || document.documentElement.scrollTop > 2400) {
+     if (document.body.scrollTop > 1800 || document.documentElement.scrollTop > 1800) {
      
         ncl[3].style.paddingRight="30px";
          ncl[3].style.opacity="1";
@@ -173,7 +167,7 @@ function scrollani() {
     }
     
     
-    if (document.body.scrollTop > 2900 || document.documentElement.scrollTop > 2900) {
+    if (document.body.scrollTop > 2100 || document.documentElement.scrollTop > 2100) {
        
          
          ncr[4].style.paddingLeft="30px";
@@ -186,13 +180,11 @@ function scrollani() {
          lcright[14].style.height="0px";
              scright[4].style.opacity="1";
         scright[4].style.top="0px";
-         
-         
-    
+
     }
     
     
-     if (document.body.scrollTop > 3100 || document.documentElement.scrollTop > 3100) {
+     if (document.body.scrollTop > 24000 || document.documentElement.scrollTop > 2400) {
      
         ncl[5].style.paddingRight="30px";
          ncl[5].style.opacity="1";
@@ -219,6 +211,156 @@ function scrollani() {
         scright[5].style.top="0px";
        
     }
+        }
+    
+    
+    
+    
+    
+    if(w<500){
+    
+     if (document.body.scrollTop > 1150 || document.documentElement.scrollTop > 1150) {
+       
+         stick_c[0].style.opacity="1";
+         ncr[0].style.paddingLeft="30px";
+         ncr[0].style.opacity="1";
+            lcright[0].style.width="0px";
+         lcright[0].style.height="0px";
+         lcright[1].style.width="0px";
+         lcright[1].style.height="0px";
+         lcright[2].style.width="0px";
+         lcright[2].style.height="0px";
+             scright[0].style.opacity="1";
+        scright[0].style.top="0px";
+
+    }
+
+    if (document.body.scrollTop > 1600 || document.documentElement.scrollTop > 1600) {
+     
+
+           ncl[1].style.paddingRight="30px";
+         ncl[1].style.opacity="1";
+          ncr[1].style.paddingLeft="30px";
+         ncr[1].style.opacity="1";
+           
+        
+            lcleft[3].style.width="0px";
+         lcleft[3].style.height="0px";
+         lcleft[4].style.width="0px";
+         lcleft[4].style.height="0px";
+         lcleft[5].style.width="0px";
+         lcleft[5].style.height="0px";
+        
+         lcright[3].style.width="0px";
+         lcright[3].style.height="0px";
+         lcright[4].style.width="0px";
+         lcright[4].style.height="0px";
+         lcright[5].style.width="0px";
+         lcright[5].style.height="0px";
+        scleft[1].style.opacity="1";
+        scleft[1].style.top="0px";
+            scright[1].style.opacity="1";
+        scright[1].style.top="0px";
+    
+        
+    }
+
+       if (document.body.scrollTop > 2250 || document.documentElement.scrollTop > 2250) {
+       
+         
+         ncr[2].style.paddingLeft="30px";
+         ncr[2].style.opacity="1";
+            lcright[6].style.width="0px";
+         lcright[6].style.height="0px";
+         lcright[7].style.width="0px";
+         lcright[7].style.height="0px";
+         lcright[8].style.width="0px";
+         lcright[8].style.height="0px";
+             scright[2].style.opacity="1";
+        scright[2].style.top="0px";
+
+    }
+    
+    
+     if (document.body.scrollTop > 2700 || document.documentElement.scrollTop > 2700) {
+     
+        ncl[3].style.paddingRight="30px";
+         ncl[3].style.opacity="1";
+          ncr[3].style.paddingLeft="30px";
+         ncr[3].style.opacity="1";
+           
+        
+            lcleft[9].style.width="0px";
+         lcleft[9].style.height="0px";
+         lcleft[10].style.width="0px";
+         lcleft[10].style.height="0px";
+         lcleft[11].style.width="0px";
+         lcleft[11].style.height="0px";
+        
+         lcright[9].style.width="0px";
+         lcright[9].style.height="0px";
+         lcright[10].style.width="0px";
+         lcright[10].style.height="0px";
+         lcright[11].style.width="0px";
+         lcright[11].style.height="0px";
+        scleft[3].style.opacity="1";
+        scleft[3].style.top="0px";
+            scright[3].style.opacity="1";
+        scright[3].style.top="0px";
+       
+    }
+    
+    
+    if (document.body.scrollTop > 3150 || document.documentElement.scrollTop > 3150) {
+       
+         
+         ncr[4].style.paddingLeft="30px";
+         ncr[4].style.opacity="1";
+            lcright[12].style.width="0px";
+         lcright[12].style.height="0px";
+         lcright[13].style.width="0px";
+         lcright[13].style.height="0px";
+         lcright[14].style.width="0px";
+         lcright[14].style.height="0px";
+             scright[4].style.opacity="1";
+        scright[4].style.top="0px";
+
+    }
+    
+    
+     if (document.body.scrollTop > 3700 || document.documentElement.scrollTop > 3700) {
+     
+        ncl[5].style.paddingRight="30px";
+         ncl[5].style.opacity="1";
+          ncr[5].style.paddingLeft="30px";
+         ncr[5].style.opacity="1";
+           
+        
+            lcleft[15].style.width="0px";
+         lcleft[15].style.height="0px";
+         lcleft[16].style.width="0px";
+         lcleft[16].style.height="0px";
+         lcleft[17].style.width="0px";
+         lcleft[17].style.height="0px";
+        
+         lcright[15].style.width="0px";
+         lcright[15].style.height="0px";
+         lcright[16].style.width="0px";
+         lcright[16].style.height="0px";
+         lcright[17].style.width="0px";
+         lcright[17].style.height="0px";
+        scleft[5].style.opacity="1";
+        scleft[5].style.top="0px";
+            scright[5].style.opacity="1";
+        scright[5].style.top="0px";
+       
+    }
+        }
+    
+    
+    
+    
+    
     
         
 }
