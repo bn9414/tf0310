40f4638548b9ba727aee1b196295555e5e3c2b0e
Bhuvanesh <bhuvaneshnick@gmail.com>
2018-02-12 11:54:18 +0530

    S

--- clients/chineselasers/common/common.js
+++ clients/chineselasers/common/common.js
@@ -49,7 +49,7 @@ if(a==2){n=1;}
          
      nav.style.boxShadow='0px 0px 0px 0px hsla(0, 0%, 0%, 0.0)';
 
-         setTimeout(function(){prod_menu.style.display="none"; main_menu.style.display='block';  },300); 
+         setTimeout(function(){prod_menu.style.display="none"; main_menu.style.display='block';  },320); 
          
        
 
@@ -79,7 +79,7 @@ n=!n;
 }
 
 function test1(){
-   setTimeout(function(){  main_menu.style.display='none';   prod_menu.style.display="block";},300); 
+   setTimeout(function(){  main_menu.style.display='none';   prod_menu.style.display="block";},320); 
     
  
     pmn[0].className='pmn mjz4 phead';
