3a4c09d630a71aefacc7134ddb23d54bd107af5c
Bhuvanesh <bhuvaneshnick@gmail.com>
2018-02-14 13:54:15 +0530

    asd

--- clients/chineselasers/offers/offers.js
+++ clients/chineselasers/offers/offers.js
@@ -5,3 +5,56 @@
 		$(".se-pre-con").fadeOut("slow");;
 	});
 //end preloads screen//
+
+var build= document.getElementsByClassName('build');
+window.onscroll = function() {scrollani()};
+  var h = parseInt(window.innerHeight);
+        var w = parseInt(window.innerWidth);
+function scrollani() {
+    /* build*/
+ if(w>500){
+    if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+          build[0].style.opacity="1";
+        build[0].style.top="10px";
+        setTimeout(function(){build[1].style.opacity="1";
+        build[1].style.top="10px";},300);
+        
+         setTimeout(function(){build[2].style.opacity="1";
+        build[2].style.top="10px";
+        },600);
+        
+        
+        
+      }
+    }
+    
+    if(w<650){
+    if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+           build[0].style.opacity="1";
+        build[0].style.top="10px";
+      }
+        
+         if (document.body.scrollTop > 1020 || document.documentElement.scrollTop > 1020) {
+         build[1].style.opacity="1";
+        build[1].style.top="10px";
+      }
+        
+            if (document.body.scrollTop > 1650 || document.documentElement.scrollTop > 1650) {
+         build[2].style.opacity="1";
+        build[2].style.top="10px";
+      }
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+  
+    
+    
+    
+}
