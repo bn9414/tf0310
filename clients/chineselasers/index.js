4875e554638f7b5c6e35369d945352cd9ee14b0d
Bhuvanesh <bhuvaneshnick@gmail.com>
2018-02-19 20:51:23 +0530

    asd

---
+++ clients/chineselasers/index.js
@@ -0,0 +1,19 @@
+//paste this code under the head tag or in a separate js file.
+	// Wait for window load
+	$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+        	
+    var hd= document.getElementsByClassName('s1hd');
+        var para= document.getElementById('jtarget');
+
+function portfullf(){
+   hd[0].style.left="0px";
+    para.style.left="0px";
+    hd[0].style.opacity="1";
+    para.style.opacity="1";
+}
+
+portfullf();
+
+    });
