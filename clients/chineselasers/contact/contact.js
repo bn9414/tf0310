b634306f46a719a2d9bb584f6118d14362c723cd
Bhuvanesh <bhuvaneshnick@gmail.com>
2018-03-05 09:46:16 +0530

    sdf
    sdf

--- clients/chineselasers/contact/contact.js
+++ clients/chineselasers/contact/contact.js
@@ -5,3 +5,27 @@
 		$(".se-pre-con").fadeOut("slow");;
 	});
 //end preloads screen//
+
+var swit1=document.getElementsByClassName('swit1');
+var switp=document.getElementsByClassName('switp');
+
+
+function grey(n){
+    
+    switp[0].style.color="#b5b5b5";
+    switp[1].style.color="#b5b5b5";
+    switp[2].style.color="#b5b5b5";
+    switp[3].style.color="#b5b5b5";
+    
+    swit1[0].className='swit1 swit1g';
+      swit1[1].className='swit1 swit1g';
+      swit1[2].className='swit1 swit1g';
+        swit1[3].className='swit1 swit1g';
+  swit1[n].className='swit1';
+    switp[n].style.color="#2193A8";
+}
+grey(0);
+swit1[0].addEventListener('click',function(){grey(0);},false);
+swit1[1].addEventListener('click',function(){grey(1);},false);
+swit1[2].addEventListener('click',function(){grey(2);},false);
+swit1[3].addEventListener('click',function(){grey(3);},false);
--- clients/chineselasers/index.js
+++ clients/chineselasers/index.js
@@ -38,6 +38,43 @@ function scrollani() {
 
 
 
+var h = parseInt(window.innerHeight);
+        var w = parseInt(window.innerWidth);
+
+var tswit= document.getElementsByClassName('tswit');
+
+var tsin= document.getElementsByClassName('tsin');
+var tall= document.getElementsByClassName('tall');
+function tswitch(n){
+    
+    tsin[0].style.backgroundColor="#fcfcfc";
+     tsin[1].style.backgroundColor="#fcfcfc";
+     tsin[2].style.backgroundColor="#fcfcfc";
+     
+    tall[0].style.display="none";tall[1].style.display="none";tall[2].style.display="none";
+    
+     tall[n].style.display="block";tsin[n].style.backgroundColor="#454545";
+    
+
+}
+if(w<880){
+tswitch(0);
+}
+tswit[0].addEventListener('click',function() {tswitch(0);}, false);
+
+tswit[1].addEventListener('click',function() {tswitch(1);}, false);
+
+tswit[2].addEventListener('click',function() {tswitch(2);}, false);
+
+
+
+
+
+
+
+
+
+
 
 
 
