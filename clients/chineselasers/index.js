3d602853aabc255ef6da2a78dacf2bf166663dc1
Bhuvanesh <bhuvaneshnick@gmail.com>
2018-03-07 09:43:43 +0530

    sdf

--- clients/chineselasers/index.js
+++ clients/chineselasers/index.js
@@ -23,23 +23,84 @@ portfullf();
 var h = parseInt(window.innerHeight);
         var w = parseInt(window.innerWidth);
 
+
+var sidehdj=document.getElementsByClassName('sidehdj');
+var mphilhd=document.getElementsByClassName('mphilhd');
+
+var psidehdj=document.getElementById('psidehdj');
+var consj1=document.getElementById('consj1');
+var consj2=document.getElementById('consj2');
+var intj=document.getElementById('intj');
+
 window.onscroll = function() {scrollani()};
 
 function scrollani() {
     /* home*/
- 
-    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+ if(w>1000){
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+ sidehdj[0].style.opacity="1";
+ sidehdj[0].style.top="130px";
+        
+    
+    }
+     if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+ mphilhd[0].style.opacity="1";
+ mphilhd[0].style.top="160px";
+        
+    
+    }
+     
+     
+     if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+
+         consj1.style.opacity="1";
+ consj1.style.left="0px";
+      consj2.style.opacity="1";
+ consj2.style.left="0px";
+    
+         
+    }
+     
+        if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
+
+         intj.style.opacity="1";
+ intj.style.left="0px";
+      
+    
+         
+    }
+     
+    
+    
  }
     
-        
+     if(w>880){
+    if (document.body.scrollTop > 1850 || document.documentElement.scrollTop > 1850) {
+
+         psidehdj.style.opacity="1";
+ psidehdj.style.top="160px";
+    
+    }
+         
+    
+    
+     }
+
+
+
+
+
+
+
 }
 
 
 
 
 
-var h = parseInt(window.innerHeight);
-        var w = parseInt(window.innerWidth);
+
+
+
 
 var tswit= document.getElementsByClassName('tswit');
 
