7d1e6380bf1d56bfbee455f52a12d69b72f52df1
Bhuvanesh <bhuvaneshnick@gmail.com>
2018-04-03 16:22:31 +0530

    jkj

---
+++ clients/marsala/event/event.js
@@ -0,0 +1,35 @@
+var slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+    
+  var slides = document.getElementsByClassName("mySlides");
+   
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+   
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+
+  slides[slideIndex-1].style.display = "block"; 
+}
+ var prev = document.getElementById('prev');
+prev.addEventListener('click',function(e){
+    plusSlides(-1);
+},false);
+
+var next = document.getElementById('next');
+next.addEventListener('click',function(e){
+    plusSlides(1);
+},false);
+
+
+ /*   showSlides(n);
+},3000);*/
