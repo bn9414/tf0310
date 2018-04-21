187f4ec02e8d82d9a242959b8cb7e40be0f39ec7
Bhuvanesh <bhuvaneshnick@gmail.com>
2018-04-21 11:42:21 +0530

    ss

--- clients/melvin/website/tour/tour.js
+++ clients/melvin/website/tour/tour.js
@@ -17,13 +17,21 @@ galaact[3].addEventListener('click',function(){
     gallery(4);
 },false);
 var galaimg = document.getElementsByClassName('gala-imgs');
+var hil = document.getElementsByClassName('hil');
+var wid = document.getElementsByClassName('wid');
+var templ = document.getElementsByClassName('templ');
+var hony = document.getElementsByClassName('hony');
+var bbhd = document.getElementsByClassName('bb-hhd');
 function gallery(g){
     if(w <= 600){}
     if(g == 1){
         galaimg[0].style.display="flex";
+        galaimg[0].style.borderBottom="3px solid #ffc527"
         galaimg[1].style.display="none";
         galaimg[2].style.display="none";
         galaimg[3].style.display="none";
+        hil[0].style.background="url(images/hillc.svg)";
+        bbhd[0].style.color="#ffc527";
     }
     else if(g == 2){
         galaimg[0].style.display="none";
