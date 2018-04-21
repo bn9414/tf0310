3fff00d4858a57686faf8cc1bac8cd987e2728ab
Bhuvanesh <bhuvaneshnick@gmail.com>
2018-04-21 13:59:55 +0530

    wsd

--- clients/melvin/website/tour/tour.js
+++ clients/melvin/website/tour/tour.js
@@ -34,23 +34,25 @@ function gallery(g){
         
         /*hil[0].style.background="url(images/hillc.svg)";
         */
+        hony[0].classList.remove('hony1');
+        
     hil[0].className="hil hil1";    
         bbhd[0].style.color="#ffc527";
         galaact[0].style.borderBottom="3px solid #ffc527";
         galaact[0].classList.add("tr-bx1");
      
         
-        wid[0].style.background="url(images/wildw.svg)";
+       /* wid[0].style.background="url(images/wildw.svg)";*/
         bbhd[1].style.color="#777";
         galaact[1].style.borderBottom="3px solid #ebeef0";
         galaact[1].classList.remove("tr-bx1");
         
-        templ[0].style.background="url(images/templew.svg)";
+        /*templ[0].style.background="url(images/templew.svg)";*/
         bbhd[2].style.color="#777";
         galaact[2].style.borderBottom="3px solid #ebeef0";
         galaact[2].classList.remove("tr-bx1");
         
-        hony[0].style.background="url(images/honeycw.svg)";
+        /*hony[0].style.background="url(images/honeycw.svg)";*/
         bbhd[3].style.color="#777";
         galaact[3].style.borderBottom="3px solid #ebeef0";
         galaact[3].classList.remove("tr-bx1");
@@ -60,23 +62,26 @@ function gallery(g){
         galaimg[1].style.display="flex";
         galaimg[2].style.display="none";
         galaimg[3].style.display="none";
+       hil[0].classList.remove('hil1');
         
-        wid[0].style.background="url(images/wildc.svg)";
+      /*  wid[0].style.background="url(images/wildc.svg)";*/
+         wid[0].className="wid wid1";
+        
         bbhd[1].style.color="#ffc527";
         galaact[1].style.borderBottom="3px solid #ffc527";
         galaact[1].classList.add("tr-bx1");
         
-        hil[0].style.background="url(images/hillw.svg)";
+       /* hil[0].style.background="url(images/hillw.svg)";*/
         bbhd[0].style.color="#777";
         galaact[0].style.borderBottom="3px solid #ebeef0";
         galaact[0].classList.remove("tr-bx1");
         
-        templ[0].style.background="url(images/templew.svg)";
+        /*templ[0].style.background="url(images/templew.svg)";*/
         bbhd[2].style.color="#777";
         galaact[2].style.borderBottom="3px solid #ebeef0";
         galaact[2].classList.remove("tr-bx1");
         
-        hony[0].style.background="url(images/honeycw.svg)";
+        /*hony[0].style.background="url(images/honeyw.svg)";*/
         bbhd[3].style.color="#777";
         galaact[3].style.borderBottom="3px solid #ebeef0";
         galaact[3].classList.remove("tr-bx1");
@@ -87,23 +92,24 @@ function gallery(g){
         galaimg[1].style.display="none";
         galaimg[2].style.display="flex";
         galaimg[3].style.display="none";
-        
-        templ[0].style.background="url(images/templec.svg)";
+         wid[0].classList.remove('wid1');
+        /*templ[0].style.background="url(images/templec.svg)";*/
+        templ[0].className="templ templ1";
         bbhd[2].style.color="#ffc527";
         galaact[2].style.borderBottom="3px solid #ffc527";
         galaact[2].classList.add("tr-bx1");
         
-        wid[0].style.background="url(images/wildw.svg)";
+        /*wid[0].style.background="url(images/wildw.svg)";*/
         bbhd[1].style.color="#777";
         galaact[1].style.borderBottom="3px solid #ebeef0";
         galaact[1].classList.remove("tr-bx1");
         
-        hil[0].style.background="url(images/hillw.svg)";
+        /*hil[0].style.background="url(images/hillw.svg)";*/
         bbhd[0].style.color="#777";
         galaact[0].style.borderBottom="3px solid #ebeef0";
         galaact[0].classList.remove("tr-bx1");
         
-        hony[0].style.background="url(images/honeycw.svg)";
+        /*hony[0].style.background="url(images/honeyw.svg)";*/
         bbhd[3].style.color="#777";
         galaact[3].style.borderBottom="3px solid #ebeef0";
         galaact[3].classList.remove("tr-bx1");
@@ -114,22 +120,25 @@ function gallery(g){
         galaimg[2].style.display="none";
         galaimg[3].style.display="flex";
         
-        hony[0].style.background="url(images/honeycc.svg)";
+     /*   hony[0].style.background="url(images/honeycc.svg)";*/
+        templ[0].classList.remove('templ1');
+        
+        hony[0].className="hony hony1";
         bbhd[3].style.color="#ffc527";
         galaact[3].style.borderBottom="3px solid #ffc527";
         galaact[3].classList.add("tr-bx1");
         
-        templ[0].style.background="url(images/templew.svg)";
+        /*templ[0].style.background="url(images/templew.svg)";*/
         bbhd[2].style.color="#777";
         galaact[2].style.borderBottom="3px solid #ebeef0";
         galaact[2].classList.remove("tr-bx1");
         
-        wid[0].style.background="url(images/wildw.svg)";
+        /*wid[0].style.background="url(images/wildw.svg)";*/
         bbhd[1].style.color="#777";
         galaact[1].style.borderBottom="3px solid #ebeef0";
         galaact[1].classList.remove("tr-bx1");
         
-        hil[0].style.background="url(images/hillw.svg)";
+        /*hil[0].style.background="url(images/hillw.svg)";*/
         bbhd[0].style.color="#777";
         galaact[0].style.borderBottom="3px solid #ebeef0";
         galaact[0].classList.remove("tr-bx1");
